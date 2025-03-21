b3583335fe386b5edab8537d9fa7ef2451e8fba9
XilefTorres <xilefjacobotorres@gmail.com>
2025-03-21 11:02:24 -0700

    pagina web v0.1

---
+++ src/pages/login.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function Login() {
+
+    const [password, setPassword] = useState("");
+    const [displayPassword, setDisplayPassword] = useState("");
+
+    const handleChange = (e : any) => {
+        const value = e.target.value; //e es el valor de todo el input
+
+        if(value.slice(-1) === "•" || value.slice(-1) === ""){ //revisa si el ultimo caracter de la contraseña es un punto
+        //                                                      (al tener mas de un valor le pone un punto con el backspace) o un espacio vacio
+            setPassword(password.slice(0, -1));  //elimina el ultimo caracter
+            setDisplayPassword(displayPassword.slice(0, -1));  
+        } else{
+            const lastChar = value.slice(-1); // Obtiene el último carácter ingresado
+            setPassword(password + lastChar);
+            setDisplayPassword("•".repeat(password.length) + lastChar);
+
+            // Oculta el último carácter después de 500ms
+            setTimeout(() => {
+                setDisplayPassword("•".repeat(password.length + 1));
+            }, 500);
+        }
+    };
+
+    return (
+        <>
+            <div className="flex flex-col gap-5 justify-center items-center h-screen">
+                <img className="size-30" 
+                src="src/assets/conta-logo.png"></img>
+                <div className="bg-white w-lg h-3/6 rounded-4xl">
+                    <h1 className="font-bold p-5 text-2xl text-center">Iniciar Sesion</h1>
+                    <div className="grid grid-cols-1 gap-5 px-5">
+                        <a className="font-medium">Usuario:</a>
+                        <input className="border-2 rounded-xl"></input>
+                        <a className="font-medium">Contraseña:</a>
+                        <input className="border-2 rounded-xl" 
+                            type="text"
+                            value={displayPassword}
+                            onChange={handleChange}/>
+                        <a className="text-red-600">Usuario o contraseña incorrectas</a>
+                    </div>
+                    <button className="bg-green-300 size-10 m-5"></button>
+                </div>
+            </div>
+        </>
+    )
+}
